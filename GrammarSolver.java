ebf51e4467170f3f003c97ac49df50c5fabccc40
Amaya <tuckeam@students.mtsd.k12.wi.us>
2022-10-19 13:41:59 -0500

    l

--- GrammarSolver.java
+++ GrammarSolver.java
@@ -2,14 +2,19 @@ import java.util.*;
 import java.io.*;
 public class GrammarSolver {
 private Map<String,String> grammarRules;
+
 public GrammarSolver(List<String> rules){
 grammarRules=new TreeMap<String,String>();
 if(rules.isEmpty()){
 throw new IllegalArgumentException();
 }
-for(int i=0;i<rules.size();i++){
+}
+ 
+public boolean grammarContains(String symbol){
+
+}
+public String getSymbols(){
     
 }
-}
-    
+
 }
