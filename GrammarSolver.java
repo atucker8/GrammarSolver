76e5b7e4178166bad7bc00dc28fe4673b43bdd70
Amaya <tuckeam@students.mtsd.k12.wi.us>
2022-10-20 13:14:50 -0500

    slay

--- GrammarSolver.java
+++ GrammarSolver.java
@@ -1,20 +1,38 @@
 import java.util.*;
 import java.io.*;
 public class GrammarSolver {
-private Map<String,String> grammarRules;
+private Map<String,String> rulesMap;
+//fix iterator(change to values)
+private Iterator<Iterator> it=rulesMap.keySet().iterator();
 
 public GrammarSolver(List<String> rules){
-grammarRules=new TreeMap<String,String>();
 if(rules.isEmpty()){
-throw new IllegalArgumentException();
+    throw new IllegalArgumentException();
+}
+rulesMap=new TreeMap<String,String>();
+String line="";
+for(int i=0;i<rules.size();i++){
+line=rules.get(i);
+String[]ruleLine=line.split("::=");
+rulesMap.put(ruleLine[0],ruleLine[1]);
 }
 }
  
 public boolean grammarContains(String symbol){
-
+return(rulesMap.containsKey(symbol));
 }
+//symbols are values
 public String getSymbols(){
-    
+    String symbols="[";
+    while(it.hasNext()){
+        symbols+=it.next()+",";
+    }
+    symbols.substring(symbols.length)
+    return "";
 }
+
+//public String[] generate(String symbol, int times){
+
+//}
 
 }
